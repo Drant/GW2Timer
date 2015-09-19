9f60c739491d7b14186a9a590193df377b587a81
Drant <drant@users.noreply.github.com>
2015-09-19 09:42:12 -0700

    Pirate Sale update

--- gw2t.js
+++ gw2t.js
@@ -11704,7 +11704,8 @@ T = {
 						var item = T.DashboardSale.Items[i];
 						var forhowmany = (item.quantity > 1) ? item.quantity + "/ " : "";
 						var prevprice = (item.pricenew < item.priceold) ? item.priceold : "";
-						$("#dsbSaleCol" + parseInt(i) % 2).append("<div class='dsbSaleEntry'>"
+						var column = (item.col !== undefined) ? item.col : parseInt(i) % 2;
+						$("#dsbSaleCol" + column).append("<div class='dsbSaleEntry'>"
 							+"<a href='" + U.convertExternalURL(item.url) + "' target='_blank'><img class='dsbSaleIcon' src='" + item.img + "' /></a> "
 							+ "<span class='dsbSalePriceOld'><del>" + forhowmany + prevprice + "</del></span> "
 							+ "<span class='dsbSalePriceNew'>" + forhowmany + item.pricenew + "<ins class='s16 s16_gem'></ins></span>"
