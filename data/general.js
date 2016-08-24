cecd0b96201bcb46008382a4fe8267d6a64a27c9
Drant <drant@users.noreply.github.com>
2016-08-23 23:35:53 -0700

    Announcement update

--- data/general.js
+++ data/general.js
@@ -1278,10 +1278,9 @@ var GW2T_DASHBOARD_DATA = {
 Announcement:
 {
 	// "<img src='img/ui/pages/account.png' /> <a href='http://gw2timer.com/?page=Catalog' title='gw2timer.com/catalog'>Account Catalog</a> browse outfits, finishers, mail carriers, gliders, nodes."
-	pve: "API services, account, WvW are offline until ArenaNet finalizes <a href='https://forum-en.guildwars2.com/forum/game/gw2/Rollback-details'>rollback</a>."
-		+ "<br/ ><a href='http://forum.renaka.com/topic/6205181'>Bloodstone-Crazed Creatures</a> guide and timings. <a href='http://gw2timer.com/?page=Creatures' title='gw2timer.com/<dfn>creatures</dfn>'>Spawn map</a> and <a href='http://gw2timer.com/?page=Slivers' title='gw2timer.com/<dfn>slivers</dfn>'>slivers</a>.",
-		//+ "<br /><a href='http://gw2timer.com/?page=WvW'>WvW livemap</a> mixed borderlands waiting for ArenaNet updated map image.",
-	wvw: "",
+	pve: "<a href='http://forum.renaka.com/topic/6209670'>Transfer Chaser</a> (2016-08-23) living story event guide.<br />"
+		+ "<a href='http://forum.renaka.com/topic/6205181'>Bloodstone-Crazed Creatures</a> guide and timings. <a href='http://gw2timer.com/?page=Creatures' title='gw2timer.com/<dfn>creatures</dfn>'>Spawn map</a> and <a href='http://gw2timer.com/?page=Slivers' title='gw2timer.com/<dfn>slivers</dfn>'>slivers</a>.",
+	wvw: "Desert map tiles for Red Borderlands will be updated later by ArenaNet.",
 	Start: new Date("2016-08-02T16:00:00Z"),
 	Finish: new Date("2016-09-16T16:00:00Z"),
 	Messages: // Greeting messages shown upon site load
