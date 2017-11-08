dc1a0365bcea1ca01bc4fdc01e304976ff9e6343
Drant <drant@users.noreply.github.com>
2017-11-07 19:50:52 -0800

    Sale update

--- data/sale.js
+++ data/sale.js
@@ -5,7 +5,7 @@
 GW2T_SALE_DATA = {
 	note: "", // Important note about the sale, optional
 	Start: new Date("2017-10-17T16:00:00Z"),
-	Finish: new Date("2017-11-07T16:00:00Z"),
+	Finish: new Date("2017-11-21T16:00:00Z"),
 	Values: {}, // ID-to-Price mapping of the promoted items, to be initialized
 	numPaddingItems: 2,
 	/*
@@ -21,25 +21,30 @@ GW2T_SALE_DATA = {
 	*/
 	Items: [
 	{i: "https://render.guildwars2.com/file/220061640ECA41C0577758030357221B4ECCE62C/502065.png", n: "Gem", p: {gem: 100}, url: "http://gw2timer.com/?page=Gem", side: 0},
+	{i: "https://i.imgur.com/PuHTFpK.png", n: "Mount Adoption License 30 Pack", p: {gem: 9600}},
 	{i: 85448, n: "Awakened Zealot Outfit", p: {gem: 700}},
-	{i: 85481, n: "Mini Choya Pumpkin Gang", p: {gem: 400}, Finish: new Date("2017-11-07T16:00:00Z")},
-	{i: 85437, n: "Mini Spooky Mounts Pack", p: {gem: 1600}, Finish: new Date("2017-11-07T16:00:00Z")},
-	{i: 67391, n: "Haunted Gramophone", p: {gem: 600}, Finish: new Date("2017-11-07T16:00:00Z")},
-	{i: 85496, n: "Riding Broom Glider", p: {gem: 500}},
-	{i: 85506, n: "Mini Elonian Familiar", p: {gem: 400}, Finish: new Date("2017-11-07T16:00:00Z")},
-	{i: 67406, n: "Candy Corn Gobbler Pack", p: {gem: 300}, Finish: new Date("2017-11-07T16:00:00Z")},
-	{i: 44069, n: "Permanent Scarecrow Finisher", p: {gem: 500}, Finish: new Date("2017-11-07T16:00:00Z")},
+	{i: 68577, n: "Arcane Outfit", p: {gem: 700}},
+	{i: 68684, n: "Crystal Nomad Outfit", p: {gem: 700}},
+	{i: 65198, n: "Fancy Winter Outfit", p: {gem: 700}},
+	{i: 67398, n: "Noble Count Outfit", p: {gem: 700}},
+	{i: 66658, n: "Shadow Assassin Outfit", p: {gem: 700}},
+	{i: 66279, n: "Ancestral Outfit", p: {gem: 700}},
+	{i: 68654, n: "Imperial Outfit", p: {gem: 700}},
+	{i: 68653, n: "Crimson Lion Dye Kit", p: {gem: 125}, discount: [[1, 125], [5, 500], [25, 2500]]},
+	{i: 81545, n: "Jormag Dye Kit", p: {gem: 125}, discount: [[1, 125], [5, 500], [25, 2500]]},
+	{i: 80974, n: "Primordus Dye Kit", p: {gem: 125}, discount: [[1, 125], [5, 500], [25, 2500]]},
+	{i: 67291, n: "Taimi's Dye Kit", p: {gem: 125}, discount: [[1, 125], [5, 500], [25, 2500]]},
+	{i: 77470, n: "Winter Chimes Dye Kit", p: {gem: 125}, discount: [[1, 125], [5, 500], [25, 2500]]},
 	{i: "https://render.guildwars2.com/file/98457F504BA2FAC8457F532C4B30EDC23929ACF9/619316.png", n: "Coin", p: {coin: 1000000}, url: "http://gw2timer.com/?page=Trading", side: 1},
 	{i: 84963, n: "War God's weapon skins#Gallery", p: {blticket: 1}},
-	{i: "https://i.imgur.com/od18Djw.png", n: "Dragon's Watch Dye Pack", p: {gem: 500}, Finish: new Date("2017-11-07T16:00:00Z")},
-	{i: 47900, n: "Metallurgic Dye Kit", p: {gem: 125}, discount: [[1, 125], [5, 500], [25, 2500]], Finish: new Date("2017-11-07T16:00:00Z")},
+	{i: "https://i.imgur.com/e5G1iUR.png", n: "Elonian Introductory Package", p: {gem: 1800}, Finish: new Date("2017-11-14T16:00:00Z")},
+	{i: 48930, n: "Consortium Harvesting Sickle", p: {gem: 1000}, Finish: new Date("2017-11-10T16:00:00Z")},
+	{i: 48955, n: "Dreamcleaver Logging Axe", p: {gem: 1000}, Finish: new Date("2017-11-10T16:00:00Z")},
+	{i: 78724, n: "Focused Solar Logging Tool", p: {gem: 1000}, Finish: new Date("2017-11-10T16:00:00Z")},
+	{i: 78909, n: "Glitter Bomb Harvesting Tool", p: {gem: 1000}, Finish: new Date("2017-11-10T16:00:00Z")},
 	{i: 80019, n: "Scruffy Backpack", p: {gem: 400}},
 	{i: 79771, n: "Fire Quiver Backpack", p: {gem: 400}},
 	{i: 68612, n: "Plush Ram Backpack", p: {gem: 300}},
-	{i: 39519, n: "Warrior Quaggan Backpack Cover", p: {gem: 300}},
-	{i: 74684, n: "Bat Wings Glider", p: {gem: 700}, Finish: new Date("2017-11-07T16:00:00Z")},
-	{i: 73939, n: "Bat Wings Backpack", p: {gem: 700}, Finish: new Date("2017-11-07T16:00:00Z")},
-	{i: 74219, n: "Dread Quiver Backpack", p: {gem: 350}, Finish: new Date("2017-11-04T16:00:00Z")},
-	{i: 69737, n: "Daydreamer's Wings Backpack", p: {gem: 500}, Finish: new Date("2017-11-04T16:00:00Z")}
+	{i: 39519, n: "Warrior Quaggan Backpack Cover", p: {gem: 300}}
 	]
 };
