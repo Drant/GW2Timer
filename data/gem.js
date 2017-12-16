b7cbea724ed027bc38d0d5e047ab66ea9f411f0e
Drant <drant@users.noreply.github.com>
2017-12-15 19:41:44 -0800

    Sale update

--- data/gem.js
+++ data/gem.js
@@ -84,7 +84,7 @@ Outfit: [
 	{i: 79671, n: "Ghostly Outfit", p: {gem: -700}},
 	{i: 79705, n: "Marjory's Shrouded Outfit", p: {gem: 700}},
 	{i: 79703, n: "Braham's Wolfblood Outfit", p: {gem: 700}},
-	{i: 80049, n: "Ice Encasement Outfit", p: {gem: -700}},
+	{i: 80049, n: "Ice Encasement Outfit", p: {gem: 700}},
 	{i: 80077, n: "Rox's Pathfinder Outfit", p: {gem: 700}},
 	{i: 80906, n: "Spring Promenade Outfit", p: {gem: -700}},
 	{i: 80973, n: "Kasmeer's Regal Outfit", p: {gem: 700}},
