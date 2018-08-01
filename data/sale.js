0c99d23888dbc08d9d7fa825157cf3263d634431
Drant <drant@users.noreply.github.com>
2018-07-31 19:37:30 -0700

    Daily update

--- data/sale.js
+++ data/sale.js
@@ -4,8 +4,8 @@
  */
 GW2T_SALE_DATA = {
 	note: "", // Important note about the sale, optional
-	Start: new Date("2018-07-17T16:00:00Z"),
-	Finish: new Date("2018-07-31T16:00:00Z"),
+	Start: new Date("2018-07-31T16:00:00Z"),
+	Finish: new Date("2018-08-21T16:00:00Z"),
 	Values: {}, // ID-to-Price mapping of the promoted items, to be initialized
 	numPaddingItems: 2,
 	/*
