55b3e1401754ac0f5579ef224621097bb0f1a3f2
Drant <drant@users.noreply.github.com>
2018-09-01 09:19:14 -0700

    Sale update

--- data/sale.js
+++ data/sale.js
@@ -21,16 +21,16 @@ GW2T_SALE_DATA = {
 	*/
 	Items: [
 	{i: "https://render.guildwars2.com/file/220061640ECA41C0577758030357221B4ECCE62C/502065.png", n: "Gem", p: {gem: 100}, url: "https://gw2timer.com/?page=Gem", side: 0},
+	{i: 88404, n: "Novelty Selection Box", p: {gem: 0}, discount: 250, Finish: new Date("2018-09-03T16:00:00Z")},
+	{i: 79698, n: "Customer Appreciation Package", p: {gem: 0}, discount: 125, Finish: new Date("2018-09-03T16:00:00Z")},
+	{i: 44602, n: "Copper-Fed Salvage-o-Matic", p: {gem: 520}, discount: 800, Finish: new Date("2018-09-03T16:00:00Z")},
+	{i: 67027, n: "Silver-Fed Salvage-o-Matic", p: {gem: 325}, discount: 500, Finish: new Date("2018-09-03T16:00:00Z")},
 	{i: 67881, n: "Aviator's Memory Box", p: {gem: 200}, discount: 400, Finish: new Date("2018-09-02T16:00:00Z")},
 	{i: 36172, n: "Bobblehead Laboratory", p: {gem: 200}, discount: 400, Finish: new Date("2018-09-02T16:00:00Z")},
 	{i: 66342, n: "Water Fight Balloon Bucket", p: {gem: 250}, discount: 500, Finish: new Date("2018-09-02T16:00:00Z")},
 	{i: 19995, n: "Bank Tab Expansion", p: {gem: 420}, discount: 600, Finish: new Date("2018-09-02T16:00:00Z")},
-	{i: 78771, n: "Electromagnetic Ascender", p: {gem: 125}, discount: 250, Finish: new Date("2018-09-01T16:00:00Z")},
-	{i: 79666, n: "Hovering Mad Mirror", p: {gem: 125}, discount: 250, Finish: new Date("2018-09-01T16:00:00Z")},
-	{i: 80854, n: "Bloodstone Ascender", p: {gem: 125}, discount: 250, Finish: new Date("2018-09-01T16:00:00Z")},
-	{i: 67356, n: "Magic Carpet", p: {gem: 125}, discount: 250, Finish: new Date("2018-09-01T16:00:00Z")},
-	{i: 49931, n: "Riding Broom", p: {gem: 125}, discount: 250, Finish: new Date("2018-09-01T16:00:00Z")},
-	{i: 49948, n: "Sonic Tunneling Tool", p: {gem: 125}, discount: 250, Finish: new Date("2018-09-01T16:00:00Z")},
+	{i: 88409, n: "Emblazoned Dragon Throne", p: {gem: 600}},
+	{i: 88462, n: "Dragon Emblem Balloon", p: {gem: 250}},
 	{i: "https://render.guildwars2.com/file/98457F504BA2FAC8457F532C4B30EDC23929ACF9/619316.png", n: "Coin", p: {coin: 1000000}, url: "https://gw2timer.com/?page=Trading", side: 1},
 	{i: 88077, n: "Desert King weapon skins#Gallery", p: {blticket: 1}},
 	{i: 88339, n: "Black Lion Backpack and Glider Voucher", p: {gem: 630}, discount: [[1, 630], [5, 2450], [10, 3500]], Finish: new Date("2018-09-04T16:00:00Z")},
@@ -41,6 +41,10 @@ GW2T_SALE_DATA = {
 	{i: 64745, n: "Ringmaster's Hat", p: {gem: 200}, Finish: new Date("2018-09-07T16:00:00Z")},
 	{i: 68574, n: "Shadow of the Dragon Helmet Skin", p: {gem: 300}, Finish: new Date("2018-09-07T16:00:00Z")},
 	{i: 68575, n: "Shadow of the Dragon Shoulder Skin", p: {gem: 300}, Finish: new Date("2018-09-07T16:00:00Z")},
-	{i: 68576, n: "Shadow of the Dragon Gloves Skin", p: {gem: 300}, Finish: new Date("2018-09-07T16:00:00Z")}
+	{i: 68576, n: "Shadow of the Dragon Gloves Skin", p: {gem: 300}, Finish: new Date("2018-09-07T16:00:00Z")},
+	{i: 64755, n: "Fuzzy Bear Hat", p: {gem: 200}, Finish: new Date("2018-09-08T16:00:00Z")},
+	{i: 64751, n: "Fuzzy Quaggan Hat with Bow", p: {gem: 200}, Finish: new Date("2018-09-08T16:00:00Z")},
+	{i: 44607, n: "Mask of the Jubilee", p: {gem: 400}, Finish: new Date("2018-09-08T16:00:00Z")},
+	{i: 44608, n: "Mask of the Queen", p: {gem: 400}, Finish: new Date("2018-09-08T16:00:00Z")}
 	]
 };
