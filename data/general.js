1ef1d5acb7a3e378a7050e7ca4fec29c74355e93
Drant <Drant@users.noreply.github.com>
2020-09-12 12:49:31 -0700

    Announcement update

--- data/general.js
+++ data/general.js
@@ -2066,11 +2066,12 @@ Announcement:
 	// "<img src='img/ui/pages/account.png' /> <a href='https://gw2timer.com/?page=Catalog' title='gw2timer.com/catalog'>Account Catalog</a> browse outfits, finishers, mail carriers, gliders, nodes."
 	// "<a href='https://gw2timer.com/?page=Audit'>Audit Account</a> to keep track of all your assets over time. <a href='https://gw2timer.com/?page=Catalog'>Catalog</a> updated for HotS release."
 	NewsPVE: { // Shown on main page dashboard
+		content: "Pact Supply spreadsheet was locked by Google somehow, it's accessible again now. Sorry for the downtime and thanks for updating it!",
 //		content: "Please tell new players about gw2timer.com!",
 //		content: "<dfn data-coord='[[16459,14575],[16369,14555],[16248,14618],[15990,14655],[15924,14414],[15742,14745],[15896,14779],[15960,14922],[16286,15063],[16371,15137],[16639,15339],[17329,15141]]'>Carving Pumpkins</dfn> in Lion's Arch, <a href='https://gw2timer.com/?page=Pumpkins'>more</a>.",
-		content: "<a href='http://gw2timer.com/?page=Orphans' title='Donation Drive Wintersday daily achievement.<br /><dfn>gw2timer.com/</dfn>orphans'>Map of orphans</a>. Talk to <dfn data-coord='[10786, 10824]'>Tixx NPC</dfn> and <dfn data-coord='[[10773, 10801],[10989, 11133],[11217, 11528]]'>trees</dfn> everyday for <a href='http://wiki.guildwars2.com/wiki/Tixx%27s_Gift'>free gifts</a>.",
+//		content: "<a href='http://gw2timer.com/?page=Orphans' title='Donation Drive Wintersday daily achievement.<br /><dfn>gw2timer.com/</dfn>orphans'>Map of orphans</a>. Talk to <dfn data-coord='[10786, 10824]'>Tixx NPC</dfn> and <dfn data-coord='[[10773, 10801],[10989, 11133],[11217, 11528]]'>trees</dfn> everyday for <a href='http://wiki.guildwars2.com/wiki/Tixx%27s_Gift'>free gifts</a>.",
 //		content: "",
-		Finish: new Date("2019-01-02T16:00:00Z")
+		Finish: new Date("2020-09-16T16:00:00Z")
 	},
 	NewsWVW: { // Shown on WvW message log
 		content: "",
