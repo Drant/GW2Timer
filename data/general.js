f8120c1e79008d962ff1c787b9c9ff2f75294565
Drant <Drant@users.noreply.github.com>
2021-10-27 10:42:25 -0700

    Halloween and DB update

--- data/general.js
+++ data/general.js
@@ -2210,17 +2210,17 @@ Countdown: {
 		Start: new Date("2018-08-09T16:00:00Z"),
 		Finish: new Date("2019-09-01T16:00:00Z")
 	},*/
-	/*{
-		isHalloween: false,
-		name_en: "Halloween 2020",
-		name_de: "Halloween 2020",
-		name_es: "Halloween 2020",
-		name_fr: "Halloween 2020",
-		name_zh: "万圣夜2020",
-		official: "news/shadow-of-the-mad-king-2020-is-live/",
-		Start: new Date("2020-10-13T16:00:00Z"),
-		Finish: new Date("2020-11-03T16:00:00Z")
-	}*/
+	{
+		isHalloween: true,
+		name_en: "Halloween 2021",
+		name_de: "Halloween 2021",
+		name_es: "Halloween 2021",
+		name_fr: "Halloween 2021",
+		name_zh: "万圣夜2021",
+		official: "news/shadow-of-the-mad-king-2021-is-live/",
+		Start: new Date("2021-10-05T16:00:00Z"),
+		Finish: new Date("2021-11-09T16:00:00Z")
+	}
 	/*{
 		name_en: "Black Friday Sale",
 		name_de: "Black Friday Verkauf",
@@ -2260,15 +2260,15 @@ Countdown: {
  */
 Story:
 {
-	isEnabled: false,
+	isEnabled: true,
 	name_en: "Mad King Says",
 	name_de: "Verrückte König sagt",
 	name_es: "Rey Loco dice",
 	name_fr: "Roi Dément a dit",
 	name_zh: "疯王说",
-	url: "https://www.guildwars2.com/en/the-game/releases/october-13-2020/",
-	Start: new Date("2020-10-13T16:00:00Z"),
-    Finish: new Date("2020-11-03T16:00:00Z")
+	url: "https://www.guildwars2.com/en/the-game/releases/october-05-2021/",
+	Start: new Date("2021-10-05T16:00:00Z"),
+	Finish: new Date("2021-11-09T16:00:00Z")
 },
 
 /*
